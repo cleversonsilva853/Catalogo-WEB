0aff623c370405359c7940f559fd84958c752ae2
Cleverson Ferreira <cleversonsilva853@gmail.com>
2026-04-10 14:23:17 -0300

    upp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Poppins", "system-ui", "sans-serif"],
-      },
+      }
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
