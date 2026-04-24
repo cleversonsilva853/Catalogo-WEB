f8272407c8661a639a47029d560406758a077e27
Cleverson Ferreira <cleversonsilva853@gmail.com>
2026-04-24 10:17:21 -0300

    up

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -216,9 +216,6 @@ const Index = () => {
           {/* Store Info */}
           <StoreInfo store={store} />
 
-          {/* Inline Install Banner for Mobile */}
-          <InstallPrompt inline />
-
           {isMenuLoading ? (
             <div className="flex justify-center py-20">
               <Loader2 className="h-10 w-10 animate-spin text-primary" />
